0005905abb890a625e197c824ca6cf159552cc31
ioane-tech <ioturman4@gmail.com>
2023-12-20 19:44:21 +0400

    add blogs page starting

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,35 +2,41 @@ import React, { useContext, useState } from 'react'
 import LoginContext from './LoginContext';
 import {Link} from "react-router-dom"
 function Navbar() {
-    const [authorized,setAuthorized]=useState(false)
+  const { isPopupOpen, 
+          setIsPopupOpen, 
+          logedIn, 
+          setLogedIn,
+          aurizedPopup,
+          setAuthorizedPopup
+        } = useContext(LoginContext);
+  const [addBlogsPage,setAddBlogsPage]=useState(false)
 
-    const { isPopupOpen, 
-            setIsPopupOpen, 
-            logedIn, 
-            setLogedIn,
-            aurizedPopup,
-            setAuthorizedPopup
-          } = useContext(LoginContext);
 
+  const loginHandler=()=>{
+    setIsPopupOpen(true)
+  }
+  const logOutHandler=()=>{
+    setLogedIn(false)
+    setAuthorizedPopup(false)
+  }
 
-    const loginHandler=()=>{
-      setIsPopupOpen(true)
-    }
-    const logOutHandler=()=>{
-      setLogedIn(false)
-      setAuthorizedPopup(false)
-    }
   return (
       <div className='navbar_div'>
-          <Link to="/"><img className='logo' src="LOGO.png" alt="" /></Link>
+              <Link onClick={()=>setAddBlogsPage(false)} to="/">
+                <div style={{marginLeft:addBlogsPage && "auto"}} className='navigate_to_home'></div>
+              </Link>
+              <img style={{marginLeft:addBlogsPage && "auto"}} className='logo' src="LOGO.png" alt="" />
           {
+            addBlogsPage==false?
               logedIn==false?
               <button className='common_button' onClick={loginHandler}>შესვლა</button>
               :
               <>
-               <Link to="/add_blogs"><button className='common_button' >დაამატე ბლოგი</button></Link>
+               <Link onClick={()=>setAddBlogsPage(true)} to="/add_blogs"><button className='common_button' >დაამატე ბლოგი</button></Link>
                <button className='common_button' onClick={logOutHandler}>გასვლა</button>
               </>
+            :
+            <></>  
           }
       </div>
   )
